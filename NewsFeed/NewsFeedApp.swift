8bca744c1a4a3a82c1cfd6fd105b8bc5cef8e671
citixenken <kmuyesu@gmail.com>
2022-04-29 16:55:18 +0300

    initial commit

---
+++ NewsFeed/NewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedApp.swift
+//  NewsFeed
+//
+//  Created by Ken Muyesu on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
